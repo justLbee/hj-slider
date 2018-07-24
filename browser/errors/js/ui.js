5f15b6264963dac4afc9cd3e4dcbdd409b381ed0
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2018-07-24 15:58:16 +0300

    errors

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,6 @@
 'use strict';
-function handleClick {
-  openMail();
-}
-const button = window.getElementById('open');
-button.onclick = handleClick();
+
+const button = document.getElementById('open');
+button.onclick = function () {
+	openMail();
+};
