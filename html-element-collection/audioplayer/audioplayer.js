2fe95df638005662894ab81083eb77837b0453bc
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2018-07-25 16:20:03 +0300

    player ready

---
+++ html-element-collection/audioplayer/audioplayer.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const player = document.getElementsByTagName('audio')[0];
+
+const playerState = document.getElementsByClassName('mediaplayer')[0];
+const btnPlay = document.getElementsByClassName('playstate')[0];
+const btnStop = document.getElementsByClassName('stop')[0];
+const btnBack = document.getElementsByClassName('back')[0];
+const btnNext = document.getElementsByClassName('next')[0];
+
+const title = document.getElementsByClassName('title')[0];
+
+const tracksArr = [
+	{
+		url: './mp3/LA Chill Tour.mp3',
+		title: 'LA Chill Tour'
+	},
+	{
+		url: './mp3/LA Fusion Jam.mp3',
+		title: 'LA Fusion Jam'
+	},
+	{
+		url: './mp3./This is it band.mp3',
+		title: 'This is it band'
+	}
+];
+
+function playerStateChange() {
+	return playerState.classList.toggle('play');
+}
+
+btnPlay.onclick = function () {
+	if (playerStateChange()) {
+		player.play();
+	}
+	else {
+		player.pause();
+	}
+};
+
+btnStop.onclick = function () {
+	player.pause();
+	player.currentTime = 0;
+
+	if(playerStateChange()) {
+		playerStateChange();
+	}
+};
+
+let firstIndex = 0;
+let lastIndex = tracksArr.length - 1;
+let currIndex = 0;
+
+btnNext.onclick = function () {
+	currIndex++;
+
+	if (currIndex > lastIndex) {
+		currIndex = firstIndex;
+	}
+
+	player.src = tracksArr[currIndex].url;
+	title.title = tracksArr[currIndex].title;
+	player.play();
+};
+
+btnBack.onclick = function () {
+	currIndex--;
+
+	if (currIndex < firstIndex) {
+		currIndex = lastIndex;
+	}
+
+	player.src = tracksArr[currIndex].url;
+	title.title = tracksArr[currIndex].title;
+	player.play();
+};
--- html-element-collection/drum-machine/drum.js
+++ html-element-collection/drum-machine/drum.js
@@ -4,7 +4,7 @@ let drumMachine = document.getElementsByTagName('audio');
 let drumButtons = document.getElementsByClassName('drum-kit__drum');
 
 for (let btn = 0; btn < drumButtons.length; btn++) {
-	drumButtons[btn].onclick = function () {
+	drumButtons[btn].onclick = () => {
 		drumMachine[btn].play();
 	}
 }
