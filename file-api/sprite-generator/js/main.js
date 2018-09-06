1dd9f5ac60041620c223f4b3bc222a5c97a79502
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2018-09-06 17:57:32 +0300

    sprite generator

--- file-api/sprite-generator/js/main.js
+++ file-api/sprite-generator/js/main.js
@@ -1,5 +1,6 @@
 const prop = ( data, name ) => data.map( item => item[ name ] ),
   summ = data => data.reduce(( total, value ) => total + value, 0 );
+
 class SpriteGenerator {
   constructor( container ) {
     this.uploadButton = container.querySelector( '.sprite-generator__upload' );
@@ -19,3 +20,50 @@ class SpriteGenerator {
 }
 
 new SpriteGenerator( document.getElementById( 'generator' ));
+
+const spriteGen = new SpriteGenerator(document.getElementById( 'generator' ));
+spriteGen.registerEvents = function() {
+  this.uploadButton.addEventListener('change', this.uploadFiles);
+  this.submitButton.addEventListener('click', this.generateSprite);
+};
+
+spriteGen.uploadFiles = function (event) {
+	console.log(this);
+
+	const files = Array.from(event.currentTarget.files);
+
+	spriteGen.updateFilesInfo(files);
+};
+
+spriteGen.updateFilesInfo = function (files) {
+	// console.log(files);
+	files.forEach(file => {
+		const img = document.createElement('img');
+
+		img.width = 50;
+		img.height = 50;
+		img.src = URL.createObjectURL(file);
+		img.name = file.name;
+
+		this.images.push(img);
+		// console.log(this.imageElement);
+	});
+};
+
+spriteGen.generateSprite = function (event) {
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
+
+	spriteGen.images.forEach(image => {
+		const pattern = ctx.createPattern(image, 'repeat');
+		ctx.beginPath();
+
+		ctx.fillStyle = pattern;
+		ctx.fillRect(0, 0, 50, 50);
+	});
+
+	spriteGen.imageElement.src = canvas.toDataURL();
+	// console.log(canvas.toDataURL());
+};
+
+spriteGen.registerEvents();
