5a78ea36b5b003d9b5488e79dc091bc11f52400b
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2018-09-12 12:09:54 +0300

    fixed

--- html-document-structure/slider/js/script.js
+++ html-document-structure/slider/js/script.js
@@ -28,47 +28,67 @@ function init() {
 
 		prev.classList.add('disabled');
 		first.classList.add('disabled');
+		prev.style = 'pointer-events:none';
+		first.style = 'pointer-events:none';
 
 		slider.addEventListener('click', changeSlide);
 
 		function changeSlide() {
+			event.preventDefault();
 			const currentSlide = container.querySelector('.slide-current');
-			let activatedSlide, currentButton, isForward, isLast;
+			let activatedSlide, currentButton;
 
 			currentButton = event.target;
 
-			if (currentButton.dataset.action === 'next' || currentButton.dataset.action === 'last') {
-				isForward = true;
-			}
-			if (currentButton.dataset.action === 'last' || currentButton.dataset.action === 'first') {
-				isLast = true;
-			}
-
-			if (!isLast) {
-				activatedSlide = isForward ? currentSlide.nextElementSibling : currentSlide.previousElementSibling;
-			} else {
-				activatedSlide = (isForward) ? slides.lastElementChild : slides.firstElementChild;
+			switch(currentButton.dataset.action){
+				case 'next':
+					activatedSlide = currentSlide.nextElementSibling;
+					break;
+				case 'prev':
+					activatedSlide = currentSlide.previousElementSibling;
+					break;
+				case	'last':
+					activatedSlide = slides.lastElementChild;
+					break;
+				case 'first':
+					activatedSlide = slides.firstElementChild;
 			}
 
 			currentSlide.classList.remove('slide-current');
 			activatedSlide.classList.add('slide-current');
 
+			updateButtons(activatedSlide);
+		}
+		
+		function updateButtons(activatedSlide) {
 			if (!activatedSlide.nextElementSibling) {
 				next.classList.add('disabled');
+				next.style = 'pointer-events:none';
+
 				last.classList.add('disabled');
+				last.style = 'pointer-events:none';
 			}
 			else {
 				next.classList.remove('disabled');
+				next.style = '';
+
 				last.classList.remove('disabled');
+				last.style = '';
 			}
 
 			if (!activatedSlide.previousElementSibling) {
 				prev.classList.add('disabled');
+				prev.style = 'pointer-events:none';
+
 				first.classList.add('disabled');
+				first.style = 'pointer-events:none';
 			}
 			else {
 				prev.classList.remove('disabled');
+				prev.style = '';
+
 				first.classList.remove('disabled');
+				first.style = '';
 			}
 		}
 	}
--- html-document-structure/todo-list/js/todo.js
+++ html-document-structure/todo-list/js/todo.js
@@ -7,7 +7,6 @@ const labels = document.querySelectorAll('input');
 Array.from(labels).forEach(label => {
 	label.addEventListener('click', e => {
 		if(!e.target.checked) {
-			console.log('hui');
 			undone.appendChild(e.target.parentElement);
 			return;
 		}
